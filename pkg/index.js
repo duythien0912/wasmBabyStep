655ae447d11219db40ed6e63eab79d6ae5281229
duythien0912 <duythien0912@gmail.com>
2018-06-07 08:41:28 +0700

    make it better

--- pkg/index.js
+++
@@ -1,4 +0,0 @@
-const js = import("./wasm_add.js");
-js.then(js => {
-  js.alert_add(3,2);
-});
--- pkg/wasm_add.d.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-export function add(arg0: number, arg1: number): number;
-
-export function alert_add(arg0: number, arg1: number): number;
-
